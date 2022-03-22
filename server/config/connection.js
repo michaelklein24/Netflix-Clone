05160b1c23b4d5843bb3d263cd5eb43e6bfd813d
Michael Klein <Michaelklein1997@gmail.com>
2022-03-21 21:48:56 -0500

    insert JAWSDB url

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config({path: require('find-config')('.env')});
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize("mysql://vuk3qzjagaws7ot4:dujo5hdi68qmsdrg@acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ypew6bbamwz2nprf", {
+  sequelize = new Sequelize(process.env.JAWSDB_URL, {
     host: 'acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
     dialectOptions: {
