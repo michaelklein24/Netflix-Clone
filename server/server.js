70af5e7f59b1ed9f52dfa07fbae53aa117543573
Michael Klein <Michaelklein1997@gmail.com>
2022-03-22 11:46:58 -0500

    added concurrently scripts

--- server/server.js
+++ server/server.js
@@ -5,22 +5,22 @@ const routes = require("./controllers");
 const sequelize = require("./config/connection");
 
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // app.use(routes);
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/build")));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/public/index.html"));
-});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/'));
+})
 
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log("Now listening at http://localhost:3000"));
+    app.listen(PORT, () => console.log("Now listening at http://localhost:3001"));
 });
