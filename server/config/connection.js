2dd1ae3093ccd7ac49d96e755b6d853f843e4d65
Michael Klein <Michaelklein1997@gmail.com>
2022-03-21 21:38:17 -0500

    server is now running

---
+++ server/config/connection.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize');
+require('dotenv').config({path: require('find-config')('.env')});
+
+let sequelize;
+
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL, {
+//     host: 'kutnpvrhom7lki7u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+//     dialect: 'mysql',
+//     dialectOptions: {
+//       decimalNumbers: true,
+//     }
+//   })
+// } else {
+
+  sequelize = new Sequelize(
+    // process.env.DB_NAME,
+    // process.env.DB_USER,
+    // process.env.DB_PASSWORD,
+    "netflix_clone",
+    "root",
+    "1234",
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+// }
+
+module.exports = sequelize;
--- server/server.js
+++ server/server.js
@@ -4,11 +4,14 @@ const express = require("express");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 
+const app = express();
+const PORT = process.env.PORT || 3000
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "./client/public/index.html")));
-app.use(routes);
+// app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    server.listen(PORT, () => console.log("Now listening"));
+    app.listen(PORT, () => console.log("Now listening at http://localhost:3000"));
 });
