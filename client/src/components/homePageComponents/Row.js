a391a297148d894d5fe7bf36a107350738fbf66e
Michael Klein <Michaelklein1997@gmail.com>
2022-03-25 14:09:02 -0500

    added title overlays when you hover over poster

--- client/src/components/homePageComponents/Row.js
+++ client/src/components/homePageComponents/Row.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import axios from "../../utils/axios";
 import YouTube from "react-youtube";
 import movieTrailer from "movie-trailer";
@@ -11,6 +11,7 @@ const APIKEY = "e434f86986f13f690168c613031343da";
 const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [trailerUrl, setTrailerUrl] = useState();
   const [movies, setMovies] = useState([]);
+  const [isShown, setIsShown] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -33,33 +34,60 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
     if (trailerUrl) {
       setTrailerUrl(undefined);
     } else {
-      console.log(movie?.name)
-      movieTrailer(null, { apiKey: APIKEY, tmdbId: movie.id})
+      console.log(movie?.name);
+      movieTrailer(null, { apiKey: APIKEY, tmdbId: movie.id })
         .then((url) => {
-          console.log(url)
+          console.log(url);
           const urlParams = new URLSearchParams(new URL(url).search);
           setTrailerUrl(urlParams.get("v"));
         })
-        .catch((err) => alert("Sorry, that video is not available.  Please select a different title."));
+        .catch((err) =>
+          alert(
+            "Sorry, that video is not available.  Please select a different title."
+          )
+        );
     }
+  };
+
+  const showCartHandler = (i) => {
+    setIsShown(i);
+  };
+
+  const hideCartHandler = () => {
+    setIsShown(-1);
   };
 
   return (
     <div className={classes.row}>
       <h2>{title}</h2>
       <div className={`${classes.row__posters}`}>
-        {movies.map((movie) => (
-          <img
-            className={`${classes.row__poster} ${
-              isLargeRow && classes.row__posterLarge
-            }`}
-            onClick={() => handleClick(movie)}
-            src={`${base_url}${
-              isLargeRow ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={movie.title}
-            key={movie.id}
-          />
+        {movies.map((movie, i) => (
+          <div
+            className={classes.poster__container}
+            onMouseLeave={hideCartHandler}
+            onMouseEnter={() => showCartHandler(i)}
+          >
+            <img
+              className={`${classes.row__poster} ${
+                isLargeRow && classes.row__posterLarge
+              }`}
+              onClick={() => handleClick(movie)}
+              src={`${base_url}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.title}
+              key={movie.id}
+            />
+            <p
+              className={
+                isShown === i
+                  ? classes.poster__title__shown
+                  : classes.poster__title__hide
+              }
+            >
+              {movie.name || movie.title}
+            </p>
+          </div>
         ))}
       </div>
       {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
